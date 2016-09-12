52c7c342fa54f5bc2cbbd75c881dec7ed784bbc1
Jayara-PC\Jayara <jajamedeiros43@gmail.com>
2016-09-12 20:07:42 -0300

    Visão - Inserir Perguntas ao questionário, Inserir Alternativas às perguntas, Escolher dinamicamente o tipo de pergunta: Subjetiva, Única Escolha ou Multipla Escolha

--- grails-app/assets/javascripts/questionarioJS.js
+++ grails-app/assets/javascripts/questionarioJS.js
@@ -1,13 +1,63 @@
 $(function(){
 
 	function cadastraPergunta(){
-
-		var tipoPergunta = $("#tipoPergunta").val();
-		var descPergunta = $("#descPergunta").val();
+		console.log("oi titia jay");
 		var $novaPergunta = $("<div/>")
-							.css("display","none")
-                            .addClass("col-md-4 col-sm-6")
-                            .addClass("jumbotron")
-	}
+			.css("display","none")
+            .addClass("jumbotron")
+            .append($('<h4 />')
+            			.text("Descrição: "))
+            .append($("<div />")
+            			.addClass("form-group")
+            			.append($("<textarea />")
+            						.addClass("form-control")
+            						.attr("rows", "5")
+            						.attr("name", "descricaoPergunta")))
+            .append($('<h4 />')
+            			.text("Tipo da pergunta: "))
+            .append($('<div/> ')
+            			.addClass("radio")
+            			.attr("id", "div-radios")
+            			.append($('<input type="radio" id="radioSubjetiva" name="tipoPergunta"> Subjetiva<br>'))
+            			.append($('<input type="radio" id="radioUnicaEscolha" name="tipoPergunta"> Única Escolha<br>'))
+						.append($('<input type="radio" id="radioMultiplaEscolha" name="tipoPergunta"> Múltipla Esscolha<br>')))
+            			.append($('<br><button type="button" id="btn-alternativa" class="btn btn-primary" disabled>Cadastrar Alternativa</button>'));
 
+    	$novaPergunta.appendTo("#div-perguntas").show();
+
+		$("input[id=radioSubjetiva]").click(function(){
+    		$("#btn-alternativa").prop("disabled",true);
+    	});
+
+    	$("input[id=radioUnicaEscolha]").click(function(){
+    		$("#btn-alternativa").prop("disabled",false);
+    	});
+
+    	$("input[id=radioMultiplaEscolha]").click(function(){
+    		$("#btn-alternativa").prop("disabled",false);
+    	});
+
+    	$("#btn-alternativa").click(cadastraAlternativa);
+    	
+    }
+
+    function cadastraAlternativa(){ 	 			
+    	console.log("opaaa");
+    	
+    	var $novaAlternativa = $("<div/>")
+    		.append($('<br><div class="form-group"><label>Descrição da Alternativa:</label><input id="descricaoAlternativa" type="text" class="form-control"><span class="glyphicon glyphicon-remove" id="remove"></span></div>'));
+
+    	$novaAlternativa.appendTo("#div-radios").show();
+
+    	$("remove").click(removeAlternativa);
+    }
+
+    function removeAlternativa(){
+    	$novaAlternativa.delete();
+    }
+
+	$("#colocar-pergunta").click(cadastraPergunta);
+	
 });
+
+
--- grails-app/views/questionario/show.gsp
+++ grails-app/views/questionario/show.gsp
@@ -6,21 +6,27 @@
     </head>
 
     <body>
+
         <div class="container">
             <div class="jumbotron">
                 <h1 class="text-center">Questionário</h1>
             </div>
+
             <div class="well">
                 <h3>Tipo do questionário:</h3>
                 <p class="bg-info">${questionario.tipo}</p>
-                <!--<h3>Clique no botão abaixo para inserir perguntas</h3>-->
                 <br>
-                <!--<input type="submit" class="btn btn-primary" value="Cadastrar Perguntas">-->
-                <button onclick="chamaPergunta()" class="btn btn-primary" id="btnChamaPergunta">Cadastrar Perguntas</button>
+                <button id="colocar-pergunta" class="btn btn-primary">Inserir Pergunta</button>
+            </div>
+
+            <div class="well" id="div-perguntas">
+                
             </div>
             
         </div>
+
     <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="assets/javascripts/questionarioJS.js"></script>
+    <script type="text/javascript" src="../../assets/questionarioJS.js"></script>
+   
     </body>
 </html>
